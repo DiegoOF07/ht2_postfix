a7b21013155a77bf9187ec041cb9ecd97fad9a62
Diego Flores <floresdiego041@gmail.com>
2024-01-20 22:27:31 -0600

    Updating read method to support multiple lines

--- Reader.java
+++ Reader.java
@@ -1,6 +1,13 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+
+/**
+ * @author Diego Flores
+ * @creationDate 20/01/2024
+ * @description Clase que se encarga de leer los datos contenidos en un archivo de texto linea por linea
+ */
 
 public class Reader {
     private File file;
@@ -10,18 +17,22 @@ public class Reader {
     }
 
     /**
-    * @description Método encargado de leer un archivo de texto y retornar sus caracteres  
+    * @description Método encargado de leer un archivo de texto y retornar sus caracteres
+    * @modification Ahora puede leer multiples lineas y retornar una lista de ellas  
     * @return ArrayList<String>
     * @throws Exception
     */
-    public char[] read() throws Exception{
-        char expresion[];
+    public ArrayList<String> read() throws Exception{
+        ArrayList<String> expresions = new ArrayList<String>();
         BufferedReader bufer = new BufferedReader(new FileReader(file));
         String line = bufer.readLine();
-        expresion = line.toCharArray();
+        while(line != null){
+            expresions.add(line);
+            line = bufer.readLine();
+        }
         if(line !=null){
             bufer.close();
         }
-        return expresion;
+        return expresions;
     }
 }
